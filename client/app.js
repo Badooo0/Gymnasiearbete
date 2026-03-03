8fc8a5fe1eb271e6180ce76268a26739a7e03b34
Badooo0 <kungen.orginal@gmail.com>
2026-03-03 21:41:21 +0100

    nu har jag fixad crud börjar på inloggning

--- client/app.js
+++ client/app.js
@@ -2,29 +2,62 @@ function Header(){
     return(
         <div>
             <h2>Cat website</h2>
-            <a href="#home">Home</a>
-            <a href="#katt">katter</a>
-            <a href="#upload">upload</a>
+            <a href="#">Home</a>
+            <a href="#katt">Katter</a>
+            <a href="#upload">Upload</a>
+            <a href="#login">Login</a>
+            <a href="#register">Register</a>
+            <a href="#logout">Logout</a>
         </div>
     );
 };
 
 
-function Upload(){
+function Upload({setKatter, raser}){
 
-    async function skickaIn(){
-        const res = await fetch("/api/upload", {
+    async function skickaIn(event){
+        event.preventDefault();
+
+        const katt = {
+            name: event.target.name.value,
+            race: event.target.race.value
+        }
+
+        const res = await fetch("/api/katter", {
             method: "POST",
-            
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(katt)
         })
-        const katter = await res.json()
+        
+        const nyKatt = await res.json()
 
+        if(!res.ok) {
+            console.log(nyKatt);
+            return;
+        } 
+        console.log(katt)
+
+        setKatter(prev=>[...prev, nyKatt])
+        event.target.reset();
     }
+
+
+
+
 
     return(
         <div id="upload" className="content">
-            <form onSubmit={skickaIn} >
-                <input type="text" name="name" id="" />
+            <form onSubmit={skickaIn}>
+                <input type="text" name="name" placeholder="Name" required/>
+                <select name="race" defaultValue="" required>
+                    <option value="" disabled>Choose a race...</option>
+                    {raser.map(r=> (
+                        <option key={r} value={r}>
+                            {r.replace(/_/g, " ").replace(/\b\w/g, bokstav => bokstav.toUpperCase())}  
+                            {/* \b = ordgräns, \w = första tecknet i ordet */}
+                        </option>
+                    ))}
+                </select>
                 <input type="submit" value="Submit" />
             </form>
         </div>
@@ -32,13 +65,11 @@ function Upload(){
 
 }
 
-function Katter(){
+function Katter({setKatter, katter, raser}){
 
     React.useEffect(()=>{
         getKatter();
     }, [])
-
-    const [katter, setKatter] = React.useState([])
 
     async function getKatter(){
         const res = await fetch("/api/katter")
@@ -49,18 +80,143 @@ function Katter(){
 
     return(
         <div id="katt" className="content">
-            {katter.map(k=> <div className="katter"><h3 key={k.id}>{k.name}</h3></div>)}
+            {katter.map(k=> 
+                <Katt katt = {k} setKatter={setKatter} raser={raser} key={k.id}/>
+            )}
         </div>
-    );
+    )
     
 };
 
+function Katt({katt, setKatter, raser}){
+    const [edit, setEdit] = React.useState(false);
+
+    async function delKatt(){
+        const res = await fetch("/api/katter/" + katt.id, {
+            method:"DELETE"
+        });
+
+        res.status.ok ? (
+            setKatter(prev=> prev.filter(k=> katt.id != k.id))
+        ) : (
+            console.log("there was a problem deleting")
+        )
+            
+            
+    }
+    
+    async function updateKatt(event){
+        event.preventDefault();
+
+        const updatedKatt = {
+            name: event.target.name.value,
+            race: event.target.race.value
+        }
+
+        const res = await fetch("/api/katter/" + katt.id, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(updatedKatt)
+        })
+
+        const nyaKatter = await res.json();
+        setKatter(prev => prev.map(k => k.id == katt.id ? nyaKatter : k));
+        setEdit(false);
+    }
+
+    function showEdit(){
+        setEdit(e=> !e)
+    }
+
+    return(
+        <div className="katter" key={katt.id}>
+            <h3>{katt.name}</h3>
+            <h5>{katt.race.replace(/_/g, " ")}</h5>
+            <button onClick={delKatt}>Delete</button>
+            <button onClick={showEdit}>{edit ? "Avbryt" : "Edit"}</button>
+            {edit && (
+                <div>
+                    <form onSubmit={updateKatt}>
+                        <input type="text" name="name" placeholder="Name" defaultValue={katt.name} required/>
+                        <select name="race" defaultValue={katt.race} required>
+                            <option value="" disabled>Choose a race...</option>
+                            {raser.map(r=> (
+                                <option key={r} value={r}>
+                                    {r.replace(/_/g, " ").replace(/\b\w/g, bokstav => bokstav.toUpperCase())}  
+                                </option>
+                            ))}
+                        </select>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            )}    
+        </div>
+    );
+
+}
+
 function App(){
+
+    const [katter, setKatter] = React.useState([])
+
+    const raser = [
+    "abyssinian", 
+    "american_bobtail", 
+    "american_curl", 
+    "american_shorthair", 
+    "american_wirehair",
+    "Balinese",
+    "Bengal_Cats",
+    "Birman",
+    "Bombay",
+    "British_Shorthair",
+    "Burmese",
+    "Burmilla",
+    "Chartreux",
+    "Chinese_Li_Hua",
+    "Colorpoint_Shorthair",
+    "Cornish_Rex",
+    "Cymric",
+    "Devon_Rex",
+    "Egyptian_Mau",
+    "European_Burmese",
+    "Exotic",
+    "Havana_Brown",
+    "Himalayan",
+    "Japanese_Bobtail",
+    "Javanese",
+    "Korat",
+    "LaPerm",
+    "Maine_Coon",
+    "Manx",
+    "Nebelung",
+    "Norwegian_Forest",
+    "Ocicat",
+    "Oriental",
+    "Persian",
+    "Pixie-Bob",
+    "Ragamuffin",
+    "Ragdoll_Cats",
+    "Russian_Blue",
+    "Savannah",
+    "Scottish_Fold",
+    "Selkirk_Rex",
+    "Siamese_Cat",
+    "Siberian",
+    "Singapura",
+    "Snowshoe",
+    "Somali",
+    "Sphynx",
+    "Tonkinese",
+    "Turkish_Angora",
+    "Turkish_Van"
+    ]
+
     return(
         <div>
             <Header />
-            <Katter />
-            <Upload />
+            <Katter setKatter={setKatter} katter={katter} raser={raser}/>
+            <Upload setKatter={setKatter} raser={raser}/>
         </div>
     );
 };
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3400;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-const fs = require("fs").promises;
+const fs = require("fs/promises")
 
 app.use(express.static("client"));
 app.use(express.json());
@@ -19,14 +19,48 @@ app.get("/api/katter", async (req, res) => {
     res.json(JSON.parse(katter));
 })
 
-app.post("/api/upload", async (req, res) => {
-    const katter = await fs.readFile("katter.json");
-    const katterna = JSON.parse(katter);
-    const katt = req.body;
+app.patch("/api/katter/:id", async (req, res) => {
+    const id = req.params.id;
+    const updateradKatt = req.body;
 
-    katt.id = "id_" + (katterna.length + 1);
+    const katter = JSON.parse(await fs.readFile("katter.json"));
+    const updatedKatter = katter.map(k => k.id == id ? {...k, ...updateradKatt}: k) /* får egenskaper från k men skriver över med uppdateradKatts egenskaper */
 
-    katterna.push(katt);
-    await fs.writeFile("katter.json", JSON.stringify(katterna, null, 2));
-    res.json(katt);
+    await fs.writeFile("katter.json", JSON.stringify(updatedKatter, null, 2));
+
+    const katt = updatedKatter.find(k => k.id == id);
+    res.json(katt)
+})
+
+app.delete("/api/katter/:id", async (req, res) => {
+    const id = req.params.id;
+    const katter = JSON.parse(await fs.readFile("katter.json"));
+    const filterKatter = katter.filter(k=> k.id != id);
+
+    await fs.writeFile("katter.json", JSON.stringify(filterKatter, null, 2));
+    res.json(filterKatter);
+})
+
+app.post("/api/katter", async (req, res) => {
+    try {
+
+        console.log("BODY RECEIVED:", req.body);
+
+        const data = await fs.readFile("katter.json");
+        const katter = JSON.parse(data);
+
+        const katt = {
+            id: "id_" + (katter.length + 1),
+            name: req.body.name,
+            race: req.body.race
+        };
+    
+        katter.push(katt)
+        await fs.writeFile("katter.json", JSON.stringify(katter, null, 2))
+        res.json(katt)
+    } catch (error) {
+        console.error("Error adding cat:", error);
+        res.status(500).json({error: "Failed to add cat"})
+    }
+   
 })
